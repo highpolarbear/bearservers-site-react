99c30205ead33f4c0498c3e0934ba4679f539590
bearlarpo <terry@highpolarbear.com>
2025-05-28 23:10:00 +0100

    added basic graphs

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -2,8 +2,76 @@ import { BaseCard } from "../../components/BaseCard";
 import { Container } from "../../components/Container";
 import GymToolsBase from "../../templates/GymToolsBase";
 import polarBearSvg from "../../assets/polar_bear.svg";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip,
+} from "chart.js";
+import { useEffect, useRef } from "react";
+
+// Register required Chart.js components
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip
+);
+
+const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+
+const datapoints_1 = [5, 45, 12, 62, 75, 36, 34];
+const datapoints_2 = [1, 4, 2, 5, 1, 4, 6, 3];
+
+const data1 = {
+  labels,
+  datasets: [
+    {
+      data: datapoints_1,
+    },
+  ],
+};
+
+const data2 = {
+  labels,
+  datasets: [
+    {
+      data: datapoints_2,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      enabled: true,
+    },
+  },
+};
 
 export const UserPage = () => {
+  const chartRef = useRef<ChartJS | null>(null);
+
+  useEffect(() => {
+    // Cleanup function to destroy the chart instance
+    return () => {
+      if (chartRef.current) {
+        chartRef.current.destroy();
+        chartRef.current = null;
+      }
+    };
+  }, []);
+
   return (
     <GymToolsBase path="/user">
       <Container>
@@ -24,8 +92,27 @@ export const UserPage = () => {
               </div>
             </BaseCard>
           </div>
-          <div className="col">Column</div>
-          <div className="col">Column</div>
+
+          <div className="col">
+            <Line
+              options={options}
+              data={data1}
+              ref={(chart) => {
+                if (chart) chartRef.current = chart;
+              }}
+            />
+          </div>
+
+          <div className="col">
+            {" "}
+            <Line
+              options={options}
+              data={data2}
+              ref={(chart) => {
+                if (chart) chartRef.current = chart;
+              }}
+            />
+          </div>
         </div>
       </Container>
     </GymToolsBase>
